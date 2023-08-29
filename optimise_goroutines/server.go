f032e172fae730e1e8210bdd60f6ba9f771cff34
drmanalo <drmanalo@outlook.com>
2023-08-29 13:18:21 +0100

    optimised goroutines

---
+++ optimise_goroutines/server.go
@@ -0,0 +1,71 @@
+package main
+
+import (
+	"log"
+	"net/http"
+	_ "net/http/pprof"
+
+	"github.com/gorilla/websocket"
+)
+
+var epoller *epoll
+
+func wsHandler(w http.ResponseWriter, r *http.Request) {
+	// Upgrade connection
+	upgrader := websocket.Upgrader{}
+	conn, err := upgrader.Upgrade(w, r, nil)
+	if err != nil {
+		return
+	}
+	if err := epoller.Add(conn); err != nil {
+		log.Printf("Failed to add connection")
+		conn.Close()
+	}
+}
+
+func main() {
+	// Enable pprof hooks
+	go func() {
+		if err := http.ListenAndServe("localhost:6060", nil); err != nil {
+			log.Fatalf("pprof failed: %v", err)
+		}
+	}()
+
+	// Start epoll
+	var err error
+	epoller, err = MkEpoll()
+	if err != nil {
+		panic(err)
+	}
+
+	go Start()
+
+	http.HandleFunc("/", wsHandler)
+	if err := http.ListenAndServe(":3000", nil); err != nil {
+		log.Fatal(err)
+	}
+}
+
+func Start() {
+	for {
+		connections, err := epoller.Wait()
+		if err != nil {
+			log.Printf("Failed to epoll wait %v", err)
+			continue
+		}
+		for _, conn := range connections {
+			if conn == nil {
+				break
+			}
+			_, msg, err := conn.ReadMessage()
+			if err != nil {
+				if err := epoller.Remove(conn); err != nil {
+					log.Printf("Failed to remove %v", err)
+				}
+				conn.Close()
+			} else {
+				log.Printf("optimised goroutines: %s", string(msg))
+			}
+		}
+	}
+}
